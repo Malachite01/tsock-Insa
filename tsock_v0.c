9ad01b6d7218a9fe334f362c5ab376734e786f97
Nacra01 <lorcanjohnson49@gmail.com>
2025-03-20 10:59:29 +0100

    Co-authored-by: Mathieu Antunes <Malachite01@users.noreply.github.com>

--- tsock_v0.c
+++ tsock_v0.c
@@ -11,20 +11,32 @@
 #include <netdb.h> /* structures retournées par les fonctions de gestion de la base de données du réseau */
 #include <stdio.h> /* pour les entrées/sorties */
 #include <errno.h> /* pour la gestion des erreurs */
+#include <arpa/inet.h>
 
+#define BUFFER_SIZE 4096
+
+//! |================|
+//! |__TOOLBOX FUNC__|
 // Fonction erreur usage
 void usage() {
 	printf("usage: cmd [-p|-s][-n ##]\n");
 	exit(1);
 }
 
+// Fonction pour gérer les erreurs
+void errorManager(int code, char *msg, int codeError) {
+	if(code == codeError) {
+		perror(msg);
+		exit(1);
+	}
+}
+
 // Fonction pour créer une adresse
-struct sockaddr_in createAdress(int port, char IP) {
+struct sockaddr_in createAdress(int port, const char* IP) {
 	struct sockaddr_in adress;
 	adress.sin_family = AF_INET; //internet
-	adress.sin_port = htons(port); //htons pour convertir le port format ordi
-	adress.sin_addr.s_addr = inet_addr(IP); //inet addr pour que l'ordi comprenne lip
-	char bufferRecep[BUFFER_SIZE]; //buffer de recep
+	adress.sin_port = htons(port); //htons pour verifier little endian big endian
+	adress.sin_addr.s_addr = inet_addr(IP); //inet addr
 	return adress;
 }
 
@@ -39,35 +51,57 @@ int createSocket(int sockType) {// sock
 	return sock;
 }
 
-void main (int argc, char **argv) {
-	//! |================|
-	//! |__DEFAULT VARS__|
+// Fonction pour construire un message correspondance fonction construire_message()
+void buildMessage(char *message, char motif, int lg) {
+	int i;
+	for (i = 0; i < lg; i++)
+		message[i] = motif;
+}
+
+// Fonction pour afficher un message correspondance fonction afficher_message()
+void printMessage(char *message, int lg) {
+	int i;
+	printf("message construit : ");
+	for (i = 0; i < lg; i++)
+	printf("%c", message[i]);
+	printf("\n");
+}
+
+int main (int argc, char **argv) {
+	//! |========|
+	//! |__VARS__|
 	int c;
 	extern char *optarg;
 	extern int optind;
-	int nb_message = -1; /* Nb de messages à envoyer ou à recevoir, par défaut : 10 en émission, infini en réception */
-	int source = -1 ; /* 0=puits, 1=source */
-	int protocolFlag = 0; /* 0=TCP, 1=UDP */
+	int nb_message = -1; // Nb de messages à envoyer ou à recevoir, par défaut : 10 en émission, infini en réception
+	int source = -1 ; // 0=puits, 1=source
+	int protocolFlag = 0; // 0=TCP, 1=UDP
 	const char* IP= "127.0.0.1";
 	int PORT = 9000;
+	char buffer[BUFFER_SIZE];
+	ssize_t dataClientSize;
+	int retcode; // code de retour des fonctions pour tester les erreurs
 
 	//! |=====================|
 	//! |__PARSING ARGUMENTS__|
 	// getopt permet de parser les options de la ligne de commande
-	while ((c = getopt(argc, argv, "pn:s")) != -1) {
+	while ((c = getopt(argc, argv, "pn:su")) != -1) {
 		switch (c) {
-			case 'p': // puit
+			case 'p': // puit (serveur)
 				if (source == 1) {
 					usage();
 				} 
 				source = 0;
+				PORT = atoi(argv[argc-1]);
 				break;
 
-			case 's': // source
+			case 's': // source (client)
 				if (source == 0) {
 					usage();
 				}
 				source = 1;
+				IP = argv[argc-2];
+				PORT = atoi(argv[argc-1]);
 				break;
 
 			case 'n': // nombre de messages
@@ -76,13 +110,6 @@ void main (int argc, char **argv) {
 
 			case 'u': // UDP
 				protocolFlag = 1;
-				// si on est dans la source on set l'IP et le port
-				if (source == 1) {
-					IP = argv[2];
-					PORT = atoi(argv[3]);
-				} else { // si on est dans le puit on set le port (pas d'ip)
-					PORT = atoi(argv[2]);
-				}
 				break;
 
 			default: // erreur
@@ -101,18 +128,55 @@ void main (int argc, char **argv) {
     
 
 	//! |=====================|
-	//! |___SOCKET CREATION___|
+	//! |___SOCKET_CREATION___|
   struct sockaddr_in adress = createAdress(PORT, IP);
   int socket;
 
-	//? TCP
-	if (protocolFlag == 0) {
-	  socket = createSocket(SOCK_STREAM);
-	} else if(protocolFlag == 1) { //? UDP
-	  socket = createSocket(SOCK_DGRAM);
-	}
+	//? en premier if TCP, en deuxieme if UDP
+	socket = (protocolFlag == 0 ? createSocket(SOCK_STREAM) : createSocket(SOCK_DGRAM));
 	
+	if(source == 1){
+		//! |====================|
+		//! |___SOURCE(CLIENT)___|
+		//* Construction du message
+		//TODO ici le 30 représente la longueur, modifier pour -l
+		char message[30];
+		nb_message = 2;
+		printf("SOURCE : lg_mesg_emis=30, port=9000, nb_envois=%d, TP=udp, dest=gauthier\n", nb_message);
 
+		for(int i = 0; i < nb_message; i++) {
+			buildMessage(message, 'a'+i, 30);
+			//TODO formater correctement et envoyer chaque message
+			// sprintf("");
+		}
+
+		//* Envoi du message avec le bon protocole
+		if (protocolFlag == 0) { //? TCP
+			
+		} else if(protocolFlag == 1) { //? UDP
+			retcode = sendto(socket, message, strlen(message), 0, (struct sockaddr *)&adress, sizeof(adress));
+			errorManager(retcode, "Erreur d'envoi", -1);	
+		}
+
+		//* Fermeture du socket
+		close(socket);
+		
+	} else if(source == 0){
+		//! |==================|
+		//! |__PUITS(SERVER)___|
+		//TODO
+		retcode = bind(socket,(struct sockaddr*)&adress,sizeof(adress)); // bind de notre socket
+		errorManager(retcode, "Erreur de bind", -1);
+
+		if (protocolFlag == 0) { //? TCP
+			
+		} else if(protocolFlag == 1) { //? UDP
+			dataClientSize = recv(socket,buffer,BUFFER_SIZE,0);
+			errorManager(dataClientSize, "Erreur de reception", -1);
+			printf("SOURCE : lg_mesg_emis=30, port=%d, nb_envois=a faire, TP=udp, dest=a faire", PORT);
+			printf("Réponse du serveur : %s\n", buffer); // Affichage du message reçu
+		}
+	}
 
 	if (nb_message != -1) {
 		if (source == 1)
@@ -125,7 +189,8 @@ void main (int argc, char **argv) {
 			printf("nb de tampons à envoyer = 10 par défaut\n");
 		} else
 		printf("nb de tampons à envoyer = infini\n");
-
 	}
+	
+	return 0;
 }
 
